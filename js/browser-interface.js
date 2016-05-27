cbdf5367ac74cc6b197cdf484484537d7d724160
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-05-27 13:32:12 -0700

    updated variable names to describe functions.

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,15 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
+  $('#userRepo').click(function() {
+    var city = $('#userName').val();
+    $('#userName').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
     .then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      $('.showRepo').text("The humidity in " + city + " is " + response.main.humidity + "%");
     })
     .fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
+      $('.showRepo').text(error.responseJSON.message);
     });
   });
 });
